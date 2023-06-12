e7ad24347df204b33ef765caf2a2629fb9bb0d9e
oscar50 <oscar@whaleandjaguar.co>
2023-06-12 11:35:34 -0500

    feat: delete create image

--- actions/actions.py
+++ actions/actions.py
@@ -15,8 +15,8 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-STUARY_API_KEY = os.environ["STUARY_API_KEY"]
+# OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+# STUARY_API_KEY = os.environ["STUARY_API_KEY"]
 FILE_IMG = os.environ["FILE_IMG"]
 
 
@@ -31,34 +31,34 @@ class ActionCreateImage(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         prompt = tracker.latest_message['text']
 
-        openai.api_key = OPENAI_API_KEY
-        response = openai.Image.create(prompt=prompt[14:],
-                                       n=1,
-                                       size="512x512")
-
-        url_img = response.get("data")[0].get("url")
-
-        img = requests.get(url_img).content
-
-        with open("img_create/img_response.jpg", 'wb') as handler:
-            handler.write(img)
-
-        url = "https://upload.estuary.tech/content/add"
-
-        payload = {}
-        headers = {'Accept': 'application/json',
-                   'Authorization': f"Bearer {STUARY_API_KEY}"}
-        files = [('data',
-                  ('file',
-                   open(f'{FILE_IMG}/img_response.jpg', 'rb'),
-                   'application/octet-stream'))]
-
-        response = requests.request("POST", url, headers=headers, data=payload, files=files)
-
-        cid = eval(response.text).get("cid")
+        # openai.api_key = OPENAI_API_KEY
+        # response = openai.Image.create(prompt=prompt[14:],
+        #                                n=1,
+        #                                size="512x512")
+        #
+        # url_img = response.get("data")[0].get("url")
+        #
+        # img = requests.get(url_img).content
+        #
+        # with open("img_create/img_response.jpg", 'wb') as handler:
+        #     handler.write(img)
+        #
+        # url = "https://upload.estuary.tech/content/add"
+        #
+        # payload = {}
+        # headers = {'Accept': 'application/json',
+        #            'Authorization': f"Bearer {STUARY_API_KEY}"}
+        # files = [('data',
+        #           ('file',
+        #            open(f'{FILE_IMG}/img_response.jpg', 'rb'),
+        #            'application/octet-stream'))]
+        #
+        # response = requests.request("POST", url, headers=headers, data=payload, files=files)
+        #
+        # cid = eval(response.text).get("cid")
 
         # dispatcher.utter_message(text=f"{prompt[14:]} image in: /img_create/img_response.jpg")
-        bot_response = {"original_message": f"{prompt}", "text": f"{cid}", "action": "create_nft"}
+        bot_response = {"original_message": f"{prompt}"}
         json_response = json.dumps(bot_response)
         dispatcher.utter_message(text=str(json_response))
 
